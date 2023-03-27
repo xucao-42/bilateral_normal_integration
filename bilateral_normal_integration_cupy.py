4ef0471eab1e53d7ffa67427ec54eee520c0be3b
Xu Cao <cao.xu@ist.osaka-u.ac.jp>
2023-03-27 14:34:09 +0900

    GPT4 commented

--- bilateral_normal_integration_cupy.py
+++ bilateral_normal_integration_cupy.py
@@ -16,37 +16,15 @@ import time
 pool = cp.cuda.MemoryPool(cp.cuda.malloc_managed)
 cp.cuda.set_allocator(pool.malloc)
 
-
-def move_left(mask):
-    return cp.pad(mask, ((0, 0), (0, 1)), "constant", constant_values=0)[:, 1:]
-
-
-def move_right(mask):
-    return cp.pad(mask, ((0, 0), (1, 0)), "constant", constant_values=0)[:, :-1]
-
-
-def move_top(mask):
-    return cp.pad(mask, ((0, 1), (0, 0)), "constant", constant_values=0)[1:, :]
-
-
-def move_bottom(mask):
-    return cp.pad(mask, ((1, 0), (0, 0)), "constant", constant_values=0)[:-1, :]
-
-
-def move_top_left(mask):
-    return cp.pad(mask, ((0, 1), (0, 1)), "constant", constant_values=0)[1:, 1:]
-
-
-def move_top_right(mask):
-    return cp.pad(mask, ((0, 1), (1, 0)), "constant", constant_values=0)[1:, :-1]
-
-
-def move_bottom_left(mask):
-    return cp.pad(mask, ((1, 0), (0, 1)), "constant", constant_values=0)[:-1, 1:]
-
-
-def move_bottom_right(mask):
-    return cp.pad(mask, ((1, 0), (1, 0)), "constant", constant_values=0)[:-1, :-1]
+# Define helper functions for moving masks in different directions
+def move_left(mask): return cp.pad(mask,((0,0),(0,1)),'constant',constant_values=0)[:,1:]  # Shift the input mask array to the left by 1, filling the right edge with zeros.
+def move_right(mask): return cp.pad(mask,((0,0),(1,0)),'constant',constant_values=0)[:,:-1]  # Shift the input mask array to the right by 1, filling the left edge with zeros.
+def move_top(mask): return cp.pad(mask,((0,1),(0,0)),'constant',constant_values=0)[1:,:]  # Shift the input mask array up by 1, filling the bottom edge with zeros.
+def move_bottom(mask): return cp.pad(mask,((1,0),(0,0)),'constant',constant_values=0)[:-1,:]  # Shift the input mask array down by 1, filling the top edge with zeros.
+def move_top_left(mask): return cp.pad(mask,((0,1),(0,1)),'constant',constant_values=0)[1:,1:]  # Shift the input mask array up and to the left by 1, filling the bottom and right edges with zeros.
+def move_top_right(mask): return cp.pad(mask,((0,1),(1,0)),'constant',constant_values=0)[1:,:-1]  # Shift the input mask array up and to the right by 1, filling the bottom and left edges with zeros.
+def move_bottom_left(mask): return cp.pad(mask,((1,0),(0,1)),'constant',constant_values=0)[:-1,1:]  # Shift the input mask array down and to the left by 1, filling the top and right edges with zeros.
+def move_bottom_right(mask): return cp.pad(mask,((1,0),(1,0)),'constant',constant_values=0)[:-1,:-1]  # Shift the input mask array down and to the right by 1, filling the top and left edges with zeros.
 
 
 def generate_dx_dy(mask, nz_horizontal, nz_vertical, step_size=1):
@@ -58,19 +36,26 @@ def generate_dx_dy(mask, nz_horizontal, nz_vertical, step_size=1):
     # o ---> horizontal positive
     num_pixel = cp.sum(mask)
 
+    # Generate an integer index array with the same shape as the mask.
     pixel_idx = cp.zeros_like(mask, dtype=int)
+    # Assign a unique integer index to each True value in the mask.
     pixel_idx[mask] = cp.arange(num_pixel)
 
+    # Create boolean masks representing the presence of neighboring pixels in each direction.
     has_left_mask = cp.logical_and(move_right(mask), mask)
     has_right_mask = cp.logical_and(move_left(mask), mask)
     has_bottom_mask = cp.logical_and(move_top(mask), mask)
     has_top_mask = cp.logical_and(move_bottom(mask), mask)
 
+    # Extract the horizontal and vertical components of the normal vectors for the neighboring pixels.
     nz_left = nz_horizontal[has_left_mask[mask]]
     nz_right = nz_horizontal[has_right_mask[mask]]
     nz_top = nz_vertical[has_top_mask[mask]]
     nz_bottom = nz_vertical[has_bottom_mask[mask]]
 
+    # Create sparse matrices representing the partial derivatives for each direction.
+    # top/bottom/left/right = vertical positive/vertical negative/horizontal negative/horizontal positive
+    # The matrices are constructed using the extracted normal components and pixel indices.
     data = cp.stack([-nz_left/step_size, nz_left/step_size], -1).flatten()
     indices = cp.stack((pixel_idx[move_left(has_left_mask)], pixel_idx[has_left_mask]), -1).flatten()
     indptr = cp.concatenate([cp.array([0]), cp.cumsum(has_left_mask[mask].astype(int) * 2)])
@@ -91,21 +76,33 @@ def generate_dx_dy(mask, nz_horizontal, nz_vertical, step_size=1):
     indptr = cp.concatenate([cp.array([0]), cp.cumsum(has_bottom_mask[mask].astype(int) * 2)])
     D_vertical_neg = csr_matrix((data, indices, indptr), shape=(num_pixel, num_pixel))
 
+    # Return the four sparse matrices representing the partial derivatives for each direction.
     return D_horizontal_pos, D_horizontal_neg, D_vertical_pos, D_vertical_neg
 
 
 def construct_facets_from(mask):
+    # Initialize an array 'idx' of the same shape as 'mask' with integers
+    # representing the indices of valid pixels in the mask.
     idx = cp.zeros_like(mask, dtype=int)
     idx[mask] = cp.arange(cp.sum(mask))
 
+    # Generate masks for neighboring pixels to define facets
     facet_move_top_mask = move_top(mask)
     facet_move_left_mask = move_left(mask)
     facet_move_top_left_mask = move_top_left(mask)
+
+    # Identify the top-left pixel of each facet by performing a logical AND operation
+    # on the masks of neighboring pixels and the input mask.
     facet_top_left_mask = facet_move_top_mask * facet_move_left_mask * facet_move_top_left_mask * mask
+
+    # Create masks for the other three vertices of each facet by shifting the top-left mask.
     facet_top_right_mask = move_right(facet_top_left_mask)
     facet_bottom_left_mask = move_bottom(facet_top_left_mask)
     facet_bottom_right_mask = move_bottom_right(facet_top_left_mask)
 
+    # Return a numpy array of facets by stacking the indices of the four vertices
+    # of each facet along the last dimension. Each row of the resulting array represents
+    # a single facet with the format [4, idx_top_left, idx_bottom_left, idx_bottom_right, idx_top_right].
     return cp.stack((4 * cp.ones(cp.sum(facet_top_left_mask).item()),
                       idx[facet_top_left_mask],
                       idx[facet_bottom_left_mask],
@@ -157,7 +154,47 @@ def bilateral_normal_integration(normal_map,
                                  tol=1e-4,
                                  cg_max_iter=5000,
                                  cg_tol=1e-3):
+    """
+    This function performs the bilateral normal integration algorithm, as described in the paper.
+    It takes as input the normal map, normal mask, and several optional parameters to control the integration process.
 
+    :param normal_map: A normal map, which is an image where each pixel's color encodes the corresponding 3D surface normal.
+    :param normal_mask: A binary mask that indicates the region of interest in the normal_map to be integrated.
+    :param k: A parameter that controls the toughness of the surface.
+              The smaller the k value, the smoother the surface appears (fewer discontinuities).
+              If set as 0, a smooth surface is obtained (No discontinuities), and the iteration should end at step 2 since the surface will not change with iterations.
+
+    :param depth_map: (Optional) An initial depth map to guide the integration process.
+    :param depth_mask: (Optional) A binary mask that indicates the valid depths in the depth_map.
+
+    :param lambda1 (Optional): A regularization parameter that controls the influence of the depth_map on the final result.
+                               Required when depth map is input.
+                               The larger the lambda1 is, the result more close to the initial depth map (fine details from the normal map are less reflected)
+
+    :param K: (Optional) A 3x3 camera intrinsic matrix, used for perspective camera models. If not provided, the algorithm assumes an orthographic camera model.
+    :param step_size: (Optional) The pixel size in the world coordinates. Default value is 1.
+                                 Used only in the orthographic camera mdoel.
+                                 Default value should be fine, unless you know the true value of the pixel size in the world coordinates.
+                                 Do not adjust it in perspective camera model.
+
+    :param max_iter: (Optional) The maximum number of iterations for the optimization process. Default value is 150.
+                                If set as 1, a smooth surface is obtained (No discontinuities).
+                                Default value should be fine.
+    :param tol:  (Optional) The tolerance for the relative change in energy to determine the convergence of the optimization process. Default value is 1e-4.
+                            The larger, the iteration stops faster, but the discontinuity preservation quality might be worse. (fewer discontinuities)
+                            Default value should be fine.
+
+    :param cg_max_iter: (Optional) The maximum number of iterations for the Conjugate Gradient solver. Default value is 5000.
+                                   Default value should be fine.
+    :param cg_tol: (Optional) The tolerance for the Conjugate Gradient solver. Default value is 1e-3.
+                              Default value should be fine.
+
+    :return: depth_map: The resulting depth map after the bilateral normal integration process.
+             surface: A pyvista PolyData mesh representing the 3D surface reconstructed from the depth map.
+             wu_map: A 2D image that represents the horizontal smoothness weight for each pixel. (green for smooth, blue/red for discontinuities)
+             wv_map: A 2D image that represents the vertical smoothness weight for each pixel. (green for smooth, blue/red for discontinuities)
+             energy_list: A list of energy values during the optimization process.
+    """
     # To avoid confusion, we list the coordinate systems in this code as follows
     #
     # pixel coordinates         camera coordinates     normal coordinates (the main paper's Fig. 1 (a))
@@ -176,6 +213,8 @@ def bilateral_normal_integration(normal_map,
     # K = [[fx, 0,  cx],
     #      [0,  fy, cy],
     #      [0,  0,  1]]
+    # I forgot why I chose the awkward coordinate system after getting used to opencv convention :(
+    # but I won't touch the working code.
     
     normal_map = cp.asarray(normal_map)
     normal_mask = cp.asarray(normal_mask)
@@ -278,6 +317,7 @@ def bilateral_normal_integration(normal_map,
     pbar = tqdm(range(max_iter))
 
     for i in pbar:
+        ################################################################################################################
         # I am manually computing A_mat = A.T @ W @ A here. It saves 2/3 time compared to the simpliest way A.T @ W @ A.
         # A.T @ W @ A can take more time than you think when the normal map become larger.
         # The diaganol matrix W=diag([wu, 1-wu, wv, 1-wv]) needs not be explicited defined in this case.
@@ -312,8 +352,8 @@ def bilateral_normal_integration(normal_map,
                                       shape=(num_normals, num_normals))
 
         A_mat_odu = A_mat_top_odu + A_mat_bottom_odu + A_mat_right_odu + A_mat_left_odu
-        A_mat = A_mat_d + A_mat_odu + A_mat_odu.T  # diganol + upper triangle + lower triangle matrix
-        ##########################################################################################################
+        A_mat = A_mat_d + A_mat_odu + A_mat_odu.T  # diagnol + upper triangle + lower triangle matrix
+        ################################################################################################################
 
         D = csr_matrix((1 / cp.clip(diagonal_data_term, 1e-5, None), pixel_idx_flat, pixel_idx_flat_indptr),
                        shape=(num_normals, num_normals))  # Jacobi preconditioner.
@@ -330,10 +370,11 @@ def bilateral_normal_integration(normal_map,
         z, _ = cg(A_mat, b_vec, x0=z, M=D, maxiter=cg_max_iter, tol=cg_tol)
         del A_mat, b_vec, wu, wv
 
-        # update weights
+        # Update weights
         wu = sigmoid((A2.dot(z)) ** 2 - (A1.dot(z)) ** 2, k)  # top
         wv = sigmoid((A4.dot(z)) ** 2 - (A3.dot(z)) ** 2, k)  # right
 
+        # Check for convergence
         energy_old = energy
         energy = cp.sum(wu * (A1.dot(z) + nx) ** 2) + \
                  cp.sum((1 - wu) * (A2.dot(z) + nx) ** 2) + \
--- bilateral_normal_integration_numpy.py
+++ bilateral_normal_integration_numpy.py
@@ -1,7 +1,7 @@
 """
 Bilateral Normal Integration (BiNI)
 """
-__author__ = "Xu Cao <cao.xu@ist.osaka-u.ac.jp>"
+__author__ = "Xu Cao <xucao.42@gmail.com>"
 __copyright__ = "Copyright (C) 2022 Xu Cao"
 __version__ = "2.0"
 
@@ -14,36 +14,15 @@ import pyvista as pv
 # from pyamg.aggregation import smoothed_aggregation_solver
 
 
-def move_left(mask):
-    return np.pad(mask, ((0, 0), (0, 1)), "constant", constant_values=0)[:, 1:]
-
-
-def move_right(mask):
-    return np.pad(mask, ((0, 0), (1, 0)), "constant", constant_values=0)[:, :-1]
-
-
-def move_top(mask):
-    return np.pad(mask, ((0, 1), (0, 0)), "constant", constant_values=0)[1:, :]
-
-
-def move_bottom(mask):
-    return np.pad(mask, ((1, 0), (0, 0)), "constant", constant_values=0)[:-1, :]
-
-
-def move_top_left(mask):
-    return np.pad(mask, ((0, 1), (0, 1)), "constant", constant_values=0)[1:, 1:]
-
-
-def move_top_right(mask):
-    return np.pad(mask, ((0, 1), (1, 0)), "constant", constant_values=0)[1:, :-1]
-
-
-def move_bottom_left(mask):
-    return np.pad(mask, ((1, 0), (0, 1)), "constant", constant_values=0)[:-1, 1:]
-
-
-def move_bottom_right(mask):
-    return np.pad(mask, ((1, 0), (1, 0)), "constant", constant_values=0)[:-1, :-1]
+# Define helper functions for moving masks in different directions
+def move_left(mask): return np.pad(mask,((0,0),(0,1)),'constant',constant_values=0)[:,1:]  # Shift the input mask array to the left by 1, filling the right edge with zeros.
+def move_right(mask): return np.pad(mask,((0,0),(1,0)),'constant',constant_values=0)[:,:-1]  # Shift the input mask array to the right by 1, filling the left edge with zeros.
+def move_top(mask): return np.pad(mask,((0,1),(0,0)),'constant',constant_values=0)[1:,:]  # Shift the input mask array up by 1, filling the bottom edge with zeros.
+def move_bottom(mask): return np.pad(mask,((1,0),(0,0)),'constant',constant_values=0)[:-1,:]  # Shift the input mask array down by 1, filling the top edge with zeros.
+def move_top_left(mask): return np.pad(mask,((0,1),(0,1)),'constant',constant_values=0)[1:,1:]  # Shift the input mask array up and to the left by 1, filling the bottom and right edges with zeros.
+def move_top_right(mask): return np.pad(mask,((0,1),(1,0)),'constant',constant_values=0)[1:,:-1]  # Shift the input mask array up and to the right by 1, filling the bottom and left edges with zeros.
+def move_bottom_left(mask): return np.pad(mask,((1,0),(0,1)),'constant',constant_values=0)[:-1,1:]  # Shift the input mask array down and to the left by 1, filling the top and right edges with zeros.
+def move_bottom_right(mask): return np.pad(mask,((1,0),(1,0)),'constant',constant_values=0)[:-1,:-1]  # Shift the input mask array down and to the right by 1, filling the top and left edges with zeros.
 
 
 def generate_dx_dy(mask, nz_horizontal, nz_vertical, step_size=1):
@@ -55,19 +34,26 @@ def generate_dx_dy(mask, nz_horizontal, nz_vertical, step_size=1):
     # o ---> horizontal positive
     num_pixel = np.sum(mask)
 
+    # Generate an integer index array with the same shape as the mask.
     pixel_idx = np.zeros_like(mask, dtype=int)
+    # Assign a unique integer index to each True value in the mask.
     pixel_idx[mask] = np.arange(num_pixel)
 
+    # Create boolean masks representing the presence of neighboring pixels in each direction.
     has_left_mask = np.logical_and(move_right(mask), mask)
     has_right_mask = np.logical_and(move_left(mask), mask)
     has_bottom_mask = np.logical_and(move_top(mask), mask)
     has_top_mask = np.logical_and(move_bottom(mask), mask)
 
+    # Extract the horizontal and vertical components of the normal vectors for the neighboring pixels.
     nz_left = nz_horizontal[has_left_mask[mask]]
     nz_right = nz_horizontal[has_right_mask[mask]]
     nz_top = nz_vertical[has_top_mask[mask]]
     nz_bottom = nz_vertical[has_bottom_mask[mask]]
 
+    # Create sparse matrices representing the partial derivatives for each direction.
+    # top/bottom/left/right = vertical positive/vertical negative/horizontal negative/horizontal positive
+    # The matrices are constructed using the extracted normal components and pixel indices.
     data = np.stack([-nz_left/step_size, nz_left/step_size], -1).flatten()
     indices = np.stack((pixel_idx[move_left(has_left_mask)], pixel_idx[has_left_mask]), -1).flatten()
     indptr = np.concatenate([np.array([0]), np.cumsum(has_left_mask[mask].astype(int) * 2)])
@@ -88,22 +74,33 @@ def generate_dx_dy(mask, nz_horizontal, nz_vertical, step_size=1):
     indptr = np.concatenate([np.array([0]), np.cumsum(has_bottom_mask[mask].astype(int) * 2)])
     D_vertical_neg = csr_matrix((data, indices, indptr), shape=(num_pixel, num_pixel))
 
+    # Return the four sparse matrices representing the partial derivatives for each direction.
     return D_horizontal_pos, D_horizontal_neg, D_vertical_pos, D_vertical_neg
 
 
 def construct_facets_from(mask):
+    # Initialize an array 'idx' of the same shape as 'mask' with integers
+    # representing the indices of valid pixels in the mask.
     idx = np.zeros_like(mask, dtype=int)
     idx[mask] = np.arange(np.sum(mask))
 
+    # Generate masks for neighboring pixels to define facets
     facet_move_top_mask = move_top(mask)
     facet_move_left_mask = move_left(mask)
     facet_move_top_left_mask = move_top_left(mask)
 
+    # Identify the top-left pixel of each facet by performing a logical AND operation
+    # on the masks of neighboring pixels and the input mask.
     facet_top_left_mask = np.logical_and.reduce((facet_move_top_mask, facet_move_left_mask, facet_move_top_left_mask, mask))
+
+    # Create masks for the other three vertices of each facet by shifting the top-left mask.
     facet_top_right_mask = move_right(facet_top_left_mask)
     facet_bottom_left_mask = move_bottom(facet_top_left_mask)
     facet_bottom_right_mask = move_bottom_right(facet_top_left_mask)
 
+    # Return a numpy array of facets by stacking the indices of the four vertices
+    # of each facet along the last dimension. Each row of the resulting array represents
+    # a single facet with the format [4, idx_top_left, idx_bottom_left, idx_bottom_right, idx_top_right].
     return np.stack((4 * np.ones(np.sum(facet_top_left_mask)),
                idx[facet_top_left_mask],
                idx[facet_bottom_left_mask],
@@ -154,6 +151,47 @@ def bilateral_normal_integration(normal_map,
                                  tol=1e-4,
                                  cg_max_iter=5000,
                                  cg_tol=1e-3):
+    """
+    This function performs the bilateral normal integration algorithm, as described in the paper.
+    It takes as input the normal map, normal mask, and several optional parameters to control the integration process.
+
+    :param normal_map: A normal map, which is an image where each pixel's color encodes the corresponding 3D surface normal.
+    :param normal_mask: A binary mask that indicates the region of interest in the normal_map to be integrated.
+    :param k: A parameter that controls the toughness of the surface.
+              The smaller the k value, the smoother the surface appears (fewer discontinuities).
+              If set as 0, a smooth surface is obtained (No discontinuities), and the iteration should end at step 2 since the surface will not change with iterations.
+
+    :param depth_map: (Optional) An initial depth map to guide the integration process.
+    :param depth_mask: (Optional) A binary mask that indicates the valid depths in the depth_map.
+
+    :param lambda1 (Optional): A regularization parameter that controls the influence of the depth_map on the final result.
+                               Required when depth map is input.
+                               The larger the lambda1 is, the result more close to the initial depth map (fine details from the normal map are less reflected)
+
+    :param K: (Optional) A 3x3 camera intrinsic matrix, used for perspective camera models. If not provided, the algorithm assumes an orthographic camera model.
+    :param step_size: (Optional) The pixel size in the world coordinates. Default value is 1.
+                                 Used only in the orthographic camera mdoel.
+                                 Default value should be fine, unless you know the true value of the pixel size in the world coordinates.
+                                 Do not adjust it in perspective camera model.
+
+    :param max_iter: (Optional) The maximum number of iterations for the optimization process. Default value is 150.
+                                If set as 1, a smooth surface is obtained (No discontinuities).
+                                Default value should be fine.
+    :param tol:  (Optional) The tolerance for the relative change in energy to determine the convergence of the optimization process. Default value is 1e-4.
+                            The larger, the iteration stops faster, but the discontinuity preservation quality might be worse. (fewer discontinuities)
+                            Default value should be fine.
+
+    :param cg_max_iter: (Optional) The maximum number of iterations for the Conjugate Gradient solver. Default value is 5000.
+                                   Default value should be fine.
+    :param cg_tol: (Optional) The tolerance for the Conjugate Gradient solver. Default value is 1e-3.
+                              Default value should be fine.
+
+    :return: depth_map: The resulting depth map after the bilateral normal integration process.
+             surface: A pyvista PolyData mesh representing the 3D surface reconstructed from the depth map.
+             wu_map: A 2D image that represents the horizontal smoothness weight for each pixel. (green for smooth, blue/red for discontinuities)
+             wv_map: A 2D image that represents the vertical smoothness weight for each pixel. (green for smooth, blue/red for discontinuities)
+             energy_list: A list of energy values during the optimization process.
+    """
     # To avoid confusion, we list the coordinate systems in this code as follows
     #
     # pixel coordinates         camera coordinates     normal coordinates (the main paper's Fig. 1 (a))
@@ -179,11 +217,12 @@ def bilateral_normal_integration(normal_map,
     projection = "orthographic" if K is None else "perspective"
     print(f"Running bilateral normal integration with k={k} in the {projection} case. \n"
           f"The number of normal vectors is {num_normals}.")
-    # transfer the normal map from the normal coordinates to the camera coordinates
+    # Transform the normal map from the normal coordinates to the camera coordinates
     nx = normal_map[normal_mask, 1]
     ny = normal_map[normal_mask, 0]
     nz = - normal_map[normal_mask, 2]
 
+    # Handle perspective and orthographic cases separately
     if K is not None:  # perspective
         img_height, img_width = normal_mask.shape[:2]
 
@@ -209,10 +248,11 @@ def bilateral_normal_integration(normal_map,
     # right, left, top, bottom
     A3, A4, A1, A2 = generate_dx_dy(normal_mask, nz_horizontal=nz_v, nz_vertical=nz_u, step_size=step_size)
 
+    # Construct the linear system
     A = vstack((A1, A2, A3, A4))
     b = np.concatenate((-nx, -nx, -ny, -ny))
 
-    # initialization
+    # Initialize variables for the optimization process
     W = spdiags(0.5 * np.ones(4*num_normals), 0, 4*num_normals, 4*num_normals, format="csr")
     z = np.zeros(np.sum(normal_mask))
     energy = (A @ z - b).T @ W @ (A @ z - b)
@@ -227,6 +267,7 @@ def bilateral_normal_integration(normal_map,
 
     pbar = tqdm(range(max_iter))
 
+    # Optimization loop
     for i in pbar:
         # fix weights and solve for depths
         A_mat = A.T @ W @ A
@@ -245,12 +286,12 @@ def bilateral_normal_integration(normal_map,
         # D = ml.aspreconditioner(cycle='W')
         z, _ = cg(A_mat, b_vec, x0=z, M=D, maxiter=cg_max_iter, tol=cg_tol)
 
-        # update weights
+        # Update the weight matrices
         wu = sigmoid((A2 @ z) ** 2 - (A1 @ z) ** 2, k)
         wv = sigmoid((A4 @ z) ** 2 - (A3 @ z) ** 2, k)
         W = spdiags(np.concatenate((wu, 1-wu, wv, 1-wv)), 0, 4*num_normals, 4*num_normals, format="csr")
 
-        # compute energy to judge whether the iteration should be terminated
+        # Check for convergence
         energy_old = energy
         energy = (A @ z - b).T @ W @ (A @ z - b)
         energy_list.append(energy)
@@ -262,6 +303,8 @@ def bilateral_normal_integration(normal_map,
     toc = time.time()
 
     print(f"Total time: {toc - tic:.3f} sec")
+
+    # Reconstruct the depth map and surface
     depth_map = np.ones_like(normal_mask, float) * np.nan
     depth_map[normal_mask] = z
 
